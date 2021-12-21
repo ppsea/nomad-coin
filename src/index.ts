73cf4c378265cfd6cd517dc5b9191ae834f1963d
ppsea <ksn569@gmail.com>
2021-12-21 23:35:43 +0900

    apply crypto js

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import * as cryptoJs from 'crypto-js'
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (index: number, previousHash: string, timeStamp: number, data: string): string => cryptoJs.SHA256(index + previousHash + timeStamp + data).toString();
 
   constructor(
     index: number,
@@ -21,3 +24,13 @@ class Block {
 }
 
 const genesisBlock: Block = new Block(0, "", "", "", 0);
+
+let blockChain: Block[] = [genesisBlock];
+
+const getBlockChain = (): Block[] => blockChain;
+
+const getLatestBlock = (): Block => blockChain[blockChain.length - 1];
+
+const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
+
+export { }
